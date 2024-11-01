df0fc705598eaa26ba83edf523ab305519cc4f0c
gavriel batyrov <gbatyrov10@gmail.com>
2024-10-31 21:14:07 -0400

    updated searchbar stylr

--- recipe/src/components/SearchBar.jsx
+++ recipe/src/components/SearchBar.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
+// src/components/SearchBar.jsx
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setSearchTerm } from '../redux/actions';
-import "./index.css";
-
+import './index.css';
 
 const SearchBar = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    const [inputValue, setInputValue] = useState('');
 
-  const handleChange = (e) => {
-    dispatch(setSearchTerm(e.target.value));
-  };
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setInputValue(value);
+        dispatch(setSearchTerm(value)); // Dispatch the search term
+    };
 
-  return (
-    <div className="seacrbar__container">
-    <input className='searchbar'
-      type="text"
-      placeholder="Search for a recipe..."
-      onChange={handleChange}
-    />
-    </div>
-  );
+    const handleFocus = () => {
+        setInputValue((prev) => prev); // Keeps the input value when focused
+    };
+
+    const handleClear = () => {
+        setInputValue('');
+        dispatch(setSearchTerm('')); // Clear the search term
+    };
+
+    return (
+        <div className="searchbar__container">
+            <input
+                className='searchbar'
+                type="text"
+                placeholder="Search for a recipe..."
+                value={inputValue}
+                onChange={handleChange}
+                onFocus={handleFocus}
+            />
+            {inputValue && (
+                <button className="clear-button" onClick={handleClear}>
+                    Clear
+                </button>
+            )}
+        </div>
+    );
 };
 
 export default SearchBar;
